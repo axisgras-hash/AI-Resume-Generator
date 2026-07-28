af6dd8d58e225b4077dadc9a06f1d42902f3ecc8
axisgras-hash <axisgras@gmail.com>
2026-07-28 11:40:54 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -32,6 +32,8 @@ GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY",type="password")
 GROQ_API_KEY = st.sidebar.text_input("GROQ_API_KEY",type="password")
 TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY",type="password")
 
+if not (GOOGLE_API_KEY) and not (GROQ_API_KEY ) and not (TAVILY_API_KEY):
+    st.sidebar.warning("PASS API KEYS")
 
 # ===============MODEL BUILDING=============
 model = ChatGoogleGenerativeAI(
