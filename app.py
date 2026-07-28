55a592bd37db23131e8a71d7c82288bd93bae7c4
axisgras-hash <axisgras@gmail.com>
2026-07-28 13:21:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,6 +115,29 @@ query = final_prompt+user_details
 
 import base64
 
+OPTIONS = ["DELHI","NOIDA","GURGAON/GURUGRAM",
+          'KANPUR','LUCKNOW','BANGLORE','PUNE]
+           
+LOCATION = st.sidebar.multiselect('SELECT LOCATION: ',
+                                    options = OPTIONS )
+JOB_PROFILE = ["PYTHON DEVELOPER",'GEN AI',
+                'FULL-STACK DEVELOPER','DATA ANALYST']
+
+PROFILE = st.sidebar.multiselect("SELECT JOB ROLE",
+                options = JOB_PROFILE)
+
+
+job_prompt = f"""Based on {PROFILE} jobs in {LOCATION}, I 
+want latest job news in using tavily, 
+try top 10 search or whatever available
+and give result like naukri theme design with
+job name, job desc, salary,
+apply link"""
+
+
+
+
+
 if st.button('generate resume'):
   with st.spinner("runnign agent"):
 
@@ -132,3 +155,9 @@ if st.button('generate resume'):
     
 
     st.html(code , width="stretch" , unsafe_allow_javascript=True)
+
+    st.divider()
+    response = agent.invoke({'messages':[{'role':'user','content':job_prompt}]})
+    
+    job_code = response['messages'][-1].content[-1]['text']
+    st.html(job_code , width="stretch" , unsafe_allow_javascript=True)
